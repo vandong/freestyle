c5648eb1fba8126f3a6f0553d3b882616d06605b
DongHa <donghv@vn.pakureserve.com>
2012-10-08 11:13:25 +0700

    upload project

---
+++ FreeStyle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FreeStyle
+//
+//  Created by DONG HA on 10/8/12.
+//  Copyright (c) 2012 DONG HA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
