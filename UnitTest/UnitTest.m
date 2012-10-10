b0ae420b283465cf68f1c8221d65084cf2be4084
vandong <van_dong9@yahoo.com>
2012-10-10 13:47:59 +0700

    update

---
+++ UnitTest/UnitTest.m
@@ -0,0 +1,32 @@
+//
+//  UnitTest.m
+//  UnitTest
+//
+//  Created by DONG HA on 10/10/12.
+//  Copyright (c) 2012 DONG HA. All rights reserved.
+//
+
+#import "UnitTest.h"
+
+@implementation UnitTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in UnitTest");
+}
+
+@end
