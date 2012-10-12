423b6ccab055728d253fe5a970272b6a637e7de7
vandong <van_dong9@yahoo.com>
2012-10-12 10:55:24 +0700

    up

---
+++ MyProj/MyProjViewController.h
@@ -0,0 +1,15 @@
+//
+//  MyProjViewController.h
+//  MyProj
+//
+//  Created by Douglas Sjoquist on 5/28/11.
+//  Copyright 2011 Ivy Gulch, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyProjViewController : UIViewController {
+    
+}
+
+@end
